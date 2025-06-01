8536fdc693c37771236acac39cb0f1b58562fc72
Oleksii <alkitav@gmail.com>
2025-06-01 20:49:12 +0000

    Updated

--- home.nix
+++ home.nix
@@ -1,10 +1,29 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
+  ngram = pkgs.fetchurl {
+    url = "https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip";
+    sha256 = "sha256-EOVIcx2fWBifw2pVP39oVwO+MNoNm7QtH3tb9fi7Iyw="; # Replace with correct hash!
+  };
   nixvim = builtins.getFlake "/home/olva/projects/olva-nixvim";
+
+  targetDir = "${config.home.homeDirectory}/data/ngrams/eng";
 in {
+  home.file."data/ngrams/1gram-a.gz".source = ngram;
+  home.activation.unpackNgrams = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p "${targetDir}"
+    echo "Extracting N-gram archive..."
+
+    # Extract only if not already extracted
+    if [ ! -d "${targetDir}" ]; then
+        ${pkgs.unzip}/bin/unzip ${ngram} -d "${targetDir}"
+    else
+      echo "Archive already extracted."
+    fi
+  '';
   home.stateVersion = "25.05";
   home.enableNixpkgsReleaseCheck = false;
   home.packages = with pkgs; [
@@ -16,6 +35,10 @@ in {
     rustlings
     eza
     lazygit
+    gzip
+    unzip
+    harper
+    golangci-lint
     (nixvim.lib.makeNixvimWithExtra builtins.currentSystem {})
   ];
   programs = {
